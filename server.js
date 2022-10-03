ab80c62aca5f8517801df86f8a58d73b83f14a04
Ajay Prajapati <ajayprajapati.prajapati661@gmai.com>
2022-10-03 10:10:06 +0530

    sever

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require("express");
+const bodyparser=require("body-parser");
+const path=require('path');
 const expressSession = require("express-session");
+
 const routes = require("./router");
 
 const oneDay = 1000 * 60 * 60 * 24;
 const app = express();
 const PORT = 9000;
 
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true}))
+
+app.use(express.static('public'))
 
 var sessionMiddlware = expressSession({
   secret: "cart",
